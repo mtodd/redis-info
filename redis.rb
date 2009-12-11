a7838fb40b62f4388f27086f6af2f109b18698a3
Matt Todd <chiology@gmail.com>
2009-12-11 02:38:17 -0500

    Added simple calculations to get more reasonable graphs (to compare with other numbers more sanely).

--- redis.rb
+++ redis.rb
@@ -1,9 +1,16 @@
 require 'yaml'
 
 class Redis < Scout::Plugin
+  KILOBYTE = 1024
+  MEGABYTE = 1048576
+  
   def build_report
     info = YAML.load(%x{redis-cli info}.gsub(/:/, ": "))
     
+    report(:used_memory_in_kb => info[:used_memory] / KILOBYTE)
+    report(:used_memory_in_mb => info[:used_memory] / MEGABYTE)
+    
+    # General Stats
     %w(used_memory changes_since_last_save).each do |key|
       report(key => info[key])
     end
